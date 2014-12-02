d061f549837eac814e1daca0535eea0a5926d2f2
jorge <jorgemarcial.alvarez@gmail.com>
2014-12-02 08:16:04 +0100

    Adding factory and manager services to manage bundle

---
+++ Model/LatchPhpSdkManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Fourcoders\Bundle\LatchBundle\Model;
+
+use Latch;
+
+class LatchPhpSdkManager
+{
+    protected $latchAppId;
+    protected $latch;
+
+    public function __construct($latchAppId , $latchAppSecret) {
+        $this->latchAppId = $latchAppId;
+        $this->latch = new Latch($latchAppId, $latchAppSecret);
+    }
+
+    public function pair($latchToken) {
+        $response = $this->latch->pair($latchToken);
+
+        return $response;
+    }
+
+    public function getStatusResponse($latchId) {
+        $statusResponse = $this->latch->status($latchId);
+
+        return $statusResponse;
+    }
+
+    public function getStatusValue($statusResponse) {
+        $appId = $this->latchAppId;
+
+        return $statusResponse->getData()->operations->$appId->status;
+    }
+}

678266462ae7f22d3a9e21370365138ecfdcb0fb
jorge <jorgemarcial.alvarez@gmail.com>
2014-12-30 09:43:18 +0100

    refactor LatchOperationVoterSpec

--- spec/Fourcoders/Bundle/LatchBundle/Model/LatchOperationVoterSpec.php
+++ spec/Fourcoders/Bundle/LatchBundle/Model/LatchOperationVoterSpec.php
@@ -16,9 +16,16 @@ class LatchOperationVoterSpec extends ObjectBehavior
     const ACCESS_DENIED = -1;
 
     public function let(
-            LatchManagerInterface $latchManager, RequestStack $requestStack, $operations
+            LatchManagerInterface $latchManager, RequestStack $requestStack, $operations,
+            TokenInterface $token, Request $request
         ) {
         $operations = array("1" => array("pattern" => "/profile", "latch_operation" => "profile-operation"));
+        $requestStack->getCurrentRequest()->willReturn($request);
+        $latchUser = $this->getMockUser();
+        $token->getUser()->willReturn($latchUser);
+        $latchManager
+            ->getOperationByName(Argument::exact("profile-operation")->getValue())
+            ->willReturn(Argument::exact("profile-operation")->getValue());
         $this->beConstructedWith($latchManager, $requestStack, $operations);
     }
 
@@ -27,50 +34,30 @@ class LatchOperationVoterSpec extends ObjectBehavior
         $this->shouldHaveType('Fourcoders\Bundle\LatchBundle\Model\LatchOperationVoter');
     }
 
-    public function it_votes_anon(TokenInterface $token, RequestStack $requestStack, Request $request)
+    public function it_votes_anon(TokenInterface $token)
     {
-        $requestStack->getCurrentRequest()->willReturn($request);
         $token->getUser()->willReturn("anon.");
         $this->vote($token, $object = null, $attributes = array())->shouldReturn(self::ACCESS_ABSTAIN);
     }
 
-    public function it_votes_that_user_logged_can_access_path_without_latch_operations(TokenInterface $token, RequestStack $requestStack, Request $request)
+    public function it_votes_that_user_logged_can_access_path_without_latch_operations(TokenInterface $token,Request $request)
     {
         $request->getPathInfo()->willReturn(Argument::exact('/home')->getValue());
-        $this->getCommonStubs($token,$requestStack,$request);
         $this->vote($token, $object = null, $attributes = array())->shouldReturn(self::ACCESS_ABSTAIN);
     }
 
-    public function it_votes_that_user_logged_can_access_path_with_unlock_latch_operations(LatchManagerInterface $latchManager, TokenInterface $token, RequestStack $requestStack, Request $request)
+    public function it_votes_that_user_logged_can_access_path_with_unlock_latch_operations(TokenInterface $token,Request $request, LatchManagerInterface $latchManager)
     {
         $request->getPathInfo()->willReturn(Argument::exact('/profile')->getValue());
-        $this->getCommonStubs($token,$requestStack,$request);
-        $this->latchManagerStub($latchManager);
         $latchManager->getOperationStatus(Argument::any(), "profile-operation")->willReturn("on");
         $this->vote($token, $object = null, $attributes = array())->shouldReturn(self::ACCESS_ABSTAIN);
     }
 
-    public function it_votes_that_user_logged_cannot_access_path_with_lock_latch_operations(LatchManagerInterface $latchManager, TokenInterface $token, RequestStack $requestStack, Request $request)
+    public function it_votes_that_user_logged_cannot_access_path_with_lock_latch_operations(TokenInterface $token,Request $request,LatchManagerInterface $latchManager)
     {
         $request->getPathInfo()->willReturn(Argument::exact('/profile')->getValue());
-        $this->getCommonStubs($token,$requestStack,$request);
-        $this->latchManagerStub($latchManager);
         $latchManager->getOperationStatus(Argument::any(), "profile-operation")->willReturn("off");
         $this->vote($token, $object = null, $attributes = array())->shouldReturn(self::ACCESS_DENIED);
-    }
-
-    protected function getCommonStubs(TokenInterface $token, RequestStack $requestStack, Request $request)
-    {
-        $requestStack->getCurrentRequest()->willReturn($request);
-        $latchUser = $this->getMockUser();
-        $token->getUser()->willReturn($latchUser);
-    }
-
-    protected function latchManagerStub(LatchManagerInterface $latchManager)
-    {
-        $latchManager
-            ->getOperationByName(Argument::exact("profile-operation")->getValue())
-            ->willReturn(Argument::exact("profile-operation")->getValue());
     }
 
     protected function getMockUser()
